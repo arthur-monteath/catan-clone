d6ac1bc655449c2c241f34da782c30d3e2edcc31
Arthur Trindade <thucatrindade1@gmail.com>
2026-07-14 00:13:01 -0300

    added tile highlighting

--- art/game/board/tiles/tile.gd
+++ art/game/board/tiles/tile.gd
@@ -1,3 +1,16 @@
 extends Node2D
 
+
 @onready var hex: Sprite2D = %HexSprite
+@onready var outline_sprite = %OutlineSprite
+
+var outline_color: Color:
+	set(v):
+		outline_color = v
+		outline_sprite.self_modulate = v
+
+@export var sand_color: Color = Color("e5b96c")
+
+
+func set_highlighted(v: bool = true):
+	outline_color = Color.WHITE if v else sand_color
